8b2b677e95175d3a23c57cabe68c29307c9045a0
Jason Stebbing <jstebbing@hbpthree.com>
2013-09-23 23:53:02 -0600

    fixed profile name validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,11 +28,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+  	user = User.new(first_name: 'Jason', last_name: 'Stebbing', email: 'jstebbing2@hbpthree.com')
+    user.password = user.password_confirmation = 'asldkfaslkd'
+
   	user.profile_name = "My Profile with Spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
-  	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  	assert !user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Jason', last_name: 'Stebbing', email: 'jstebbing2@hbpthree.com')
+    user.password = user.password_confirmation = 'asldkfaslkd'
+
+    user.profile_name = 'jstebbing_1'
+    assert user.valid?
   end
 end
